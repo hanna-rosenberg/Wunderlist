ebde87bb5cc0a62e1077fa870b1634cf7cc70b11
Deliciaes <ceramstedt@gmail.com>
2021-12-16 16:19:30 +0100

    implemented create account function

--- signup.php
+++ signup.php
@@ -10,19 +10,20 @@ require __DIR__ . '/views/header.php';
 <!--login form-->
 <div class="createAccount">
     Create an account:
-    <form>
-    </form action="/app/users/register.php" method="POST">
 
-    <label for="name">Name</label>
-    <input type="text" id="name" name="name" required>
+    <form action="/app/users/register.php" method="POST">
 
-    <label for="email">Email</label>
-    <input type="email" id="email" name="email" required>
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" required>
 
-    <label for="password">Password</label>
-    <input type="password" id="password" name="password" required>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" required>
 
-    <button type="submit">Create Account</button>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+
+        <button type="submit">Create Account</button>
+    </form>
 </div>
 
 <?php
