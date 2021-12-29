41684194a0589eccd3331c0a8e4987ef9d8050ad
Deliciaes <ceramstedt@gmail.com>
2021-12-29 13:59:06 +0100

    continued on tasks
    i changed the database slightly to include user id in tasks for safer filtering

--- app/users/createtask.php
+++ app/users/createtask.php
@@ -9,17 +9,16 @@ if (isset($_POST['taskName'], $_POST['taskDescription'])) {
     $taskName = trim($_POST['taskName']);
     $taskDescription = trim($_POST['taskDescription']);
     $taskDeadline = $_POST['taskDeadline'];
-    $taskCompleted = 'false';
-    $listId = $_POST['listId'];
-    $listName = $_POST['listName'];
+    $taskCompleted = false;
+    $userId = $_SESSION['user']['id'];
 
-    $statement = $database->prepare('INSERT INTO tasks(task, description, deadline, completed, list_id) VALUES (:task, :description, :deadline, :completed, :list_id)');
+    $statement = $database->prepare('INSERT INTO tasks(task, description, deadline, completed, user_id) VALUES (:task, :description, :deadline, :completed, :user_id)');
     $statement->bindParam(':task', $taskName);
     $statement->bindParam(':description', $taskDescription);
     $statement->bindParam(':deadline', $taskDeadline);
     $statement->bindParam(':completed', $taskCompleted);
-    $statement->bindParam(':list_id', $listId);
+    $statement->bindParam(':user_id', $userId);
     $statement->execute();
 }
 
-redirect('/tasks.php?listId=' . $listId . '&listName=' . $listName);
+redirect('/wunderlists.php');
--- wunderlists.php
+++ wunderlists.php
@@ -9,17 +9,7 @@ require __DIR__ . '/views/header.php';
 
 ?>
 
-Create a list<br>
-
-<form action="/app/users/createlist.php" method="POST">
-
-    <label for="listName">List Name</label>
-    <input type="text" id="listName" name="listName" required>
-
-    <button type="submit">Create List</button>
-</form>
-<br>
-Your lists:
+<h2>Your lists:</h2>
 <?php
 $statement = $database->prepare('SELECT * FROM lists WHERE user_id = :user_id');
 $statement->bindParam(':user_id', $_SESSION['user']['id'], PDO::PARAM_STR);
@@ -33,10 +23,60 @@ $lists = $statement->fetchAll(PDO::FETCH_DEFAULT);
     <?php endfor; ?>
 </ul>
 
+Create a list<br>
 
+<form action="/app/users/createlist.php" method="POST">
 
+    <label for="listName">List Name</label>
+    <input type="text" id="listName" name="listName" required>
 
+    <button type="submit">Create List</button>
+</form>
+<br>
 
+<div class="tasks-parent">
+
+    <h2>Tasks </h2>
+
+    Create a task<br>
+
+    <form action="/app/users/createtask.php" method="POST">
+
+        <label for="taskName">Task name</label>
+        <input type="text" id="taskName" name="taskName" required>
+
+        <label for="taskDescription">Task description</label>
+        <input type="text" id="taskDescription" name="taskDescription" required>
+
+        <label for="taskDeadline">Deadline</label>
+        <input type="date" id="taskDeadline" name="taskDeadline">
+
+        <button type="submit">Create Task</button>
+    </form>
+    <br>
+</div>
 
 <?php
-require __DIR__ . '/views/footer.php';
+
+
+$statement = $database->prepare('SELECT * FROM tasks WHERE user_id = :user_id');
+$statement->bindParam(':user_id', $_SESSION['user']['id'], PDO::PARAM_STR);
+$statement->execute();
+
+$tasks = $statement->fetchAll(PDO::FETCH_DEFAULT);
+
+
+for ($i = 0; $i < count($tasks); $i++) : ?>
+    <div class="task">
+        <article>
+            <div><b><?php echo $tasks[$i]['task'] ?></b></div>
+            <div><?php echo $tasks[$i]['description'] ?></div>
+            <div>Deadline: <?php echo $tasks[$i]['deadline'] ?></div>
+            <div>Completed: <?php echo $tasks[$i]['completed'] ?></div>
+            <button>Edit</button>
+        </article>
+    <?php endfor; ?>
+
+    <?php
+
+    require __DIR__ . '/views/footer.php';
