0b1ebcb59ea58dec96fb665b6418aee7f9182212
Emma Ramstedt <71063351+Deliciaes@users.noreply.github.com>
2022-01-12 22:59:52 +0100

    lint

--- views/successmsg.php
+++ views/successmsg.php
@@ -1,4 +1,5 @@
 <?php
 if (isset($_SESSION['successMsg'])) : ?>
-    <div class="alert alert-success" role="alert"><?php echo $_SESSION['successMsg']; ?></div><?php unset($_SESSION['successMsg']);
-                                                                                            endif; ?>
+    <div class="alert alert-success" role="alert"><?php echo $_SESSION['successMsg'];
+                                                    unset($_SESSION['successMsg']);
+                                                endif; ?></div>
--- views/warningmsg.php
+++ views/warningmsg.php
@@ -1,4 +1,5 @@
 <?php
 if (isset($_SESSION['warningMsg'])) : ?>
-    <div class="alert alert-warning" role="alert"><?php echo $_SESSION['warningMsg']; ?></div><?php unset($_SESSION['successMsg']);
-                                                                                            endif; ?>
+    <div class="alert alert-warning" role="alert"><?php echo $_SESSION['warningMsg'];
+                                                    unset($_SESSION['successMsg']);
+                                                endif; ?></div>
