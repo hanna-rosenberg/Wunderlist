0269269c04ada7713e1cd49fb59521a7923c2966
Deliciaes <ceramstedt@gmail.com>
2022-01-12 16:13:12 +0100

    bugfix

--- assets/scripts/dialogues.js
+++ assets/scripts/dialogues.js
@@ -2,7 +2,8 @@
 const alerts = document.querySelectorAll('.alert');
 
 alerts.forEach((alertMsg) => {
-  if (trim(alertMsg.innerHTML) != '') {
+  //i used console log to look at the lenght of the alert div when it is empty which returned 5. if it increases it means theres an error message displayed. the div will become visible
+  if (alertMsg.innerHTML.length >= 6) {
     alertMsg.classList.remove('hidden');
   }
 });
